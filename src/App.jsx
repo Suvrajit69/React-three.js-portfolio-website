f7b0e22446dd02d8ee5b3dbe106dfb5ef5d16278
Suvrajit Mondal <rdxarijitmondal.com>
2023-08-20 23:13:10 +0530

    project complete

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  Hero,
+  Navbar,
+  About,
+  Experience,
+  Works,
+  Tech,
+  FeedBacks,
+  Contact,
+} from "./components";
+import  {StarsCanvas}  from "./components/canvas";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        {/* <FeedBacks /> */}
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import About from "./About";
+import Experience from "./Experience";
+import FeedBacks from "./FeedBacks";
+import Contact from "./Contact";
+import Tech from "./Tech";
+import Works from "./Works";
+import Loader from "./Loader";
+
+export {
+  Hero,
+  Navbar,
+  About,
+  Experience,
+  Works,
+  Tech,
+  FeedBacks,
+  Contact,
+  Loader
+};
